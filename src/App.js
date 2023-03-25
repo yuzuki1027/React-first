f485df1fc0128ecafa0124d4e56e4d6adf9e9c8b
yuzuki1027 <yuzuki1027@gmail.com>
2023-03-25 15:34:28 +0900

    App編集

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Footer from './footer';
 import Header from './header';
-
+import Main from './main';
+import Map from './map';
+import Spot from './spot';
+import Article from './Article ';
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
+      <Article />
+      <Map />
+      <Spot />
+      <Map />
       <Footer />
     </div>
   );
