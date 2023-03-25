185166db92dc200a08051e08f917b21b85a391d8
yuzuki1027 <yuzuki1027@gmail.com>
2023-03-25 15:44:23 +0900

    mainの中にarticle,soit入れ込み

--- src/main/index.jsx
+++ src/main/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Article from '../article';
+import Spot from '../spot';
 
 const Main = () => {
   return (
-    <div className='main'></div>
+    <div className='main'>
+      <Article />
+      <Spot />
+    </div>
   )
 }
 
