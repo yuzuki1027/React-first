076284bdc73953139e605d8dbcd7fc183b463cd0
yuzuki1027 <yuzuki1027@gmail.com>
2023-03-25 14:55:57 +0900

    footer作った

--- src/footer/index.jsx
+++ src/footer/index.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='footer'></div>
+    <footer className='footer'>
+      &copy; react練習
+     PRODUCT BY ....
+    </footer>
   )
 }
 
