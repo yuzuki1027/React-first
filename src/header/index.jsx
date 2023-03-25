bd789d6134c31ddd2896a9509efa73026cd893d6
yurayui <yuka1020toshi@icloud.com>
2023-03-25 15:47:30 +0900

    header css

--- src/header/index.jsx
+++ src/header/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from "./New Zealand.png";
+import "./style.css";
 
 const Header = () => {
   return (
     <header>
-      <img class="logo" src={logo}/>
+      <img class="logo" src={logo} height={100} />
     </header>
   )
 }
